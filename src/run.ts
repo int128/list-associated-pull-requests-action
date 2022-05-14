8ed2b691d9f3564fac89b7eb055d54f32f6516ee
Hidetake Iwata <int128@gmail.com>
2022-05-14 17:02:16 +0900

    Group pull request by sub paths

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { findAssociation } from './history'
+import { Association, findAssociation } from './history'
 import { getCommit } from './queries/commit'
 import { getAssociatedPullRequestsInCommitHistoryOfSubTreeQuery } from './queries/history'
 
@@ -9,6 +9,7 @@ type Inputs = {
   base: string
   head: string
   path: string
+  groupBySubPaths: string[]
 }
 
 type Outputs = {
@@ -44,8 +45,46 @@ export const run = async (inputs: Inputs): Promise<Outputs> => {
   core.endGroup()
 
   const association = findAssociation(history, baseCommit.repository.object.oid)
+  if (inputs.groupBySubPaths.length === 0) {
+    return {
+      body: [...association.pullOrCommits].map((s) => `- ${s}`).join('\n'),
+      associatedPullRequests: [...association.pulls],
+      pullRequestListMarkdown: [...association.pulls].map((s) => `- #${s}`).join('\n'),
+    }
+  }
+
+  const subPathAssociations = new Map<string, Association>()
+  for (const subPath of inputs.groupBySubPaths) {
+    const history = await getAssociatedPullRequestsInCommitHistoryOfSubTreeQuery(octokit, {
+      owner: github.context.repo.owner,
+      name: github.context.repo.repo,
+      expression: inputs.head,
+      path: subPath,
+      since: baseCommit.repository.object.committedDate,
+    })
+    core.startGroup(`Commit history of subpath ${subPath}`)
+    core.info(JSON.stringify(history, undefined, 2))
+    core.endGroup()
+    const association = findAssociation(history, baseCommit.repository.object.oid)
+    subPathAssociations.set(subPath, association)
+  }
+
+  const others = new Set(association.pullOrCommits)
+  const body = []
+  for (const [subPath, association] of subPathAssociations) {
+    body.push(`### ${subPath}`)
+    for (const pullOrCommit of association.pullOrCommits) {
+      body.push(`- ${pullOrCommit}`)
+      others.delete(pullOrCommit)
+    }
+  }
+  body.push(`### Others`)
+  for (const pullOrCommit of others) {
+    body.push(`- ${pullOrCommit}`)
+  }
+
   return {
-    body: [...association.pullOrCommits].map((s) => `- ${s}`).join('\n'),
+    body: body.join('\n'),
     associatedPullRequests: [...association.pulls],
     pullRequestListMarkdown: [...association.pulls].map((s) => `- #${s}`).join('\n'),
   }
