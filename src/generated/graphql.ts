22301b7a9dbbda9eee321c650b75bc5cf492a6eb
Hidetake Iwata <int128@gmail.com>
2022-05-28 19:25:00 +0900

    Support group-by-sub-paths for pull request

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -28,4 +28,15 @@ export type PullRequestCommitsQueryVariables = Types.Exact<{
 }>;
 
 
-export type PullRequestCommitsQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number } | null, repository?: { __typename?: 'Repository', pullRequest?: { __typename?: 'PullRequest', commits: { __typename?: 'PullRequestCommitConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasNextPage: boolean, endCursor?: string | null }, nodes?: Array<{ __typename?: 'PullRequestCommit', commit: { __typename?: 'Commit', oid: string, associatedPullRequests?: { __typename?: 'PullRequestConnection', nodes?: Array<{ __typename?: 'PullRequest', number: number } | null> | null } | null } } | null> | null } } | null } | null };
+export type PullRequestCommitsQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number } | null, repository?: { __typename?: 'Repository', pullRequest?: { __typename?: 'PullRequest', headRefOid: string, commits: { __typename?: 'PullRequestCommitConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasNextPage: boolean, endCursor?: string | null }, nodes?: Array<{ __typename?: 'PullRequestCommit', commit: { __typename?: 'Commit', oid: string, committedDate: string, associatedPullRequests?: { __typename?: 'PullRequestConnection', nodes?: Array<{ __typename?: 'PullRequest', number: number } | null> | null } | null } } | null> | null } } | null } | null };
+
+export type CommitHistoryOfSubTreeQueryVariables = Types.Exact<{
+  owner: Types.Scalars['String'];
+  name: Types.Scalars['String'];
+  oid: Types.Scalars['GitObjectID'];
+  path: Types.Scalars['String'];
+  since: Types.Scalars['GitTimestamp'];
+}>;
+
+
+export type CommitHistoryOfSubTreeQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number } | null, repository?: { __typename?: 'Repository', object?: { __typename: 'Blob' } | { __typename: 'Commit', history: { __typename?: 'CommitHistoryConnection', nodes?: Array<{ __typename?: 'Commit', oid: string } | null> | null } } | { __typename: 'Tag' } | { __typename: 'Tree' } | null } | null };
--- src/pull.ts
+++ src/pull.ts
@@ -1,12 +1,12 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { findChangeSetFromPullRequestCommitsQueryList } from './history'
+import { PullRequestCommitsQuery, CommitHistoryOfSubTreeQuery } from './generated/graphql'
 import { getPullRequestCommits } from './queries/pull'
+import { getCommitHistoryOfSubTree } from './queries/subtree'
 
 type Inputs = {
   token: string
   pullRequest: number
-  path: string
   groupBySubPaths: string[]
 }
 
@@ -19,9 +19,6 @@ export const computeChangeSetOfPullRequest = async (inputs: Inputs): Promise<Out
   if (inputs.groupBySubPaths.length > 0) {
     core.warning(`group-by-sub-paths is not yet supported for pull-request`)
   }
-  if (inputs.path !== '.') {
-    core.warning(`path is not yet supported for pull-request`)
-  }
 
   const octokit = github.getOctokit(inputs.token)
 
@@ -33,10 +30,123 @@ export const computeChangeSetOfPullRequest = async (inputs: Inputs): Promise<Out
   core.startGroup(`Commit of pull request #${inputs.pullRequest}`)
   core.info(JSON.stringify(commitsList, undefined, 2))
   core.endGroup()
-  const changeSet = findChangeSetFromPullRequestCommitsQueryList(commitsList)
+
+  const pullRequestCommits = parsePullRequestCommitsQueryList(commitsList)
+
+  const subPathCommits = new Map<string, Set<string>>()
+  for (const subPath of inputs.groupBySubPaths) {
+    const q = await getCommitHistoryOfSubTree(octokit, {
+      owner: github.context.repo.owner,
+      name: github.context.repo.repo,
+      oid: pullRequestCommits.headRefOid,
+      path: subPath,
+      since: pullRequestCommits.earliestCommittedDate.toISOString(),
+    })
+    core.startGroup(`Commit history of sub tree: ${subPath}`)
+    core.info(JSON.stringify(q, undefined, 2))
+    core.endGroup()
+    const commits = parseCommitHistoryOfSubTreeQuery(q)
+    subPathCommits.set(subPath, commits)
+  }
+
+  const subPathCommitMetadatas = new Map<string, Map<string, CommitMetadata>>()
+  const otherCommitMetadatas = new Map(pullRequestCommits.commitMetadatas)
+  for (const [subPath, oids] of subPathCommits) {
+    const commitMetadataInSubPath = new Map<string, CommitMetadata>()
+    for (const oid of oids) {
+      otherCommitMetadatas.delete(oid)
+      const commitMetadata = pullRequestCommits.commitMetadatas.get(oid)
+      if (commitMetadata !== undefined) {
+        commitMetadataInSubPath.set(subPath, commitMetadata)
+      }
+    }
+    if (commitMetadataInSubPath.size > 0) {
+      subPathCommitMetadatas.set(subPath, commitMetadataInSubPath)
+    }
+  }
+
+  const body = []
+  for (const [subPath, metadataMap] of subPathCommitMetadatas) {
+    body.push(`### ${subPath}`)
+    for (const [oid, metadata] of metadataMap) {
+      body.push(metadata.pull ? `#${metadata.pull}` : oid)
+    }
+  }
+  if (otherCommitMetadatas.size > 0) {
+    body.push(`### Others`)
+    for (const [oid, metadata] of otherCommitMetadatas) {
+      body.push(metadata.pull ? `#${metadata.pull}` : oid)
+    }
+  }
 
   return {
-    body: [...changeSet.pullOrCommits].map((s) => `- ${s}`).join('\n'),
-    associatedPullRequests: [...changeSet.pulls],
+    body: body.join('\n'),
+    associatedPullRequests: [...pullRequestCommits.pulls],
   }
+}
+
+type CommitMetadata = {
+  pull?: number
+}
+
+type PullRequestCommits = {
+  commitMetadatas: Map<string, CommitMetadata>
+  pulls: Set<number>
+  earliestCommittedDate: Date
+  headRefOid: string
+}
+
+const parsePullRequestCommitsQueryList = (queryList: PullRequestCommitsQuery[]): PullRequestCommits => {
+  const commits = new Map<string, CommitMetadata>()
+  const pulls = new Set<number>()
+  let earliestCommittedDate: Date | undefined
+
+  const headRefOid = queryList[0].repository?.pullRequest?.headRefOid
+
+  for (const q of queryList) {
+    for (const node of q.repository?.pullRequest?.commits?.nodes ?? []) {
+      if (node?.commit.oid === undefined) {
+        continue
+      }
+      const committedDate = new Date(node.commit.committedDate)
+      if (earliestCommittedDate === undefined || committedDate < earliestCommittedDate) {
+        earliestCommittedDate = committedDate
+      }
+      if (!node.commit.associatedPullRequests?.nodes?.length) {
+        core.info(`${node.commit.oid} -> no pull request`)
+        commits.set(node.commit.oid, {})
+        continue
+      }
+      for (const pull of node.commit.associatedPullRequests.nodes) {
+        if (pull == null) {
+          continue
+        }
+        core.info(`${node.commit.oid} -> #${pull.number}`)
+        commits.set(node.commit.oid, { pull: pull.number })
+        pulls.add(pull.number)
+      }
+    }
+  }
+
+  if (earliestCommittedDate === undefined) {
+    throw new Error(`internal error: earliestCommittedDate === undefined`)
+  }
+  if (headRefOid === undefined) {
+    throw new Error(`internal error: headRefOid === undefined`)
+  }
+  return { commitMetadatas: commits, pulls, earliestCommittedDate, headRefOid }
+}
+
+const parseCommitHistoryOfSubTreeQuery = (q: CommitHistoryOfSubTreeQuery): Set<string> => {
+  if (q.repository?.object?.__typename !== 'Commit') {
+    throw new Error(`internal error: __typename !== Commit`)
+  }
+  const commits = new Set<string>()
+  for (const node of q.repository.object.history.nodes ?? []) {
+    if (node == null) {
+      continue
+    }
+    commits.add(node.oid)
+  }
+  return commits
 }
