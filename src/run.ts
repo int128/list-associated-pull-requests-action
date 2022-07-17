183bcb5acc6e3a88d04f3fd8de6c0e96d5196a02
Hidetake Iwata <int128@gmail.com>
2022-07-17 19:10:24 +0900

    Refactor

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,13 @@
-import { computeChangeSetBetweenBaseHead } from './basehead'
-import { computeChangeSetOfPullRequest } from './pull'
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { Commit } from './history'
+import { listAssociatedPullRequests } from './list'
 
 type Inputs = {
   token: string
-  pullRequest: number
-  base: string
-  head: string
+  pullRequest?: number
+  base?: string
+  head?: string
   groupByPaths: string[]
   showOthersGroup: boolean
 }
@@ -15,15 +17,51 @@ type Outputs = {
 }
 
 export const run = async (inputs: Inputs): Promise<Outputs> => {
-  inputs.groupByPaths = sanitizePaths(inputs.groupByPaths)
+  const octokit = github.getOctokit(inputs.token)
+  const groupByPaths = sanitizePaths(inputs.groupByPaths)
 
+  let { base, head } = inputs
   if (inputs.pullRequest) {
-    return await computeChangeSetOfPullRequest(inputs)
+    const { data: pull } = await octokit.rest.pulls.get({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: inputs.pullRequest,
+    })
+    core.info(`Found #${pull.number}`)
+    base = pull.base.ref
+    head = pull.head.ref
   }
-  if (inputs.base && inputs.head) {
-    return await computeChangeSetBetweenBaseHead(inputs)
+  if (!base || !head) {
+    throw new Error('you need to set either pull-request or base/head')
   }
-  throw new Error('you need to set either pull-request or base/head')
+
+  const associatedPulls = await listAssociatedPullRequests(octokit, {
+    base,
+    head,
+    showOthersGroup: inputs.showOthersGroup,
+    groupByPaths,
+  })
+  const body = []
+  for (const [path, commits] of associatedPulls.commitsByPath) {
+    body.push(`### ${path}`)
+    body.push(...formatCommits(commits))
+  }
+  if (associatedPulls.others) {
+    body.push(`### Others`)
+    body.push(...formatCommits(associatedPulls.others))
+  }
+  return { body: body.join('\n') }
 }
 
 const sanitizePaths = (groupByPaths: string[]) => groupByPaths.filter((p) => p.length > 0 && !p.startsWith('#'))
+
+const formatCommits = (commits: Commit[]): string[] => [
+  ...new Set(
+    commits.map((commit) => {
+      if (commit.pull) {
+        return `- #${commit.pull.number} @${commit.pull.author}`
+      }
+      return `- ${commit.commitId}`
+    })
+  ),
+]
