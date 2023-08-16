3d14738cc85940625f135776d9fdb96518e95625
Hidetake Iwata <int128@gmail.com>
2023-08-16 13:36:01 +0900

    Remove pr-98

--- tests/github-integration.test.ts
+++ tests/github-integration.test.ts
@@ -2,26 +2,13 @@ import assert from 'assert'
 import { run } from '../src/run'
 
 const { INTEGRATION_TEST_GITHUB_TOKEN } = process.env
-const describeIfGitHubToken = INTEGRATION_TEST_GITHUB_TOKEN ? describe : describe.skip
+const describeOnlyIfToken = INTEGRATION_TEST_GITHUB_TOKEN ? describe : describe.skip
 
-describeIfGitHubToken('GitHub integration tests', () => {
-  // https://github.com/int128/list-associated-pull-requests-action/pull/98
-  it('should generate outputs of pr-98', async () => {
-    assert(INTEGRATION_TEST_GITHUB_TOKEN)
-    const outputs = await run({
-      owner: 'int128',
-      repo: 'list-associated-pull-requests-action',
-      token: INTEGRATION_TEST_GITHUB_TOKEN,
-      pullRequest: 98,
-      groupByPaths: ['src', 'tests', '.github'],
-      showOthersGroup: true,
-    })
-    expect(outputs).toMatchSnapshot()
-  })
-
-  // https://github.com/int128/list-associated-pull-requests-action/pull/491
+describeOnlyIfToken('GitHub integration test', () => {
   it('should generate outputs of pr-491', async () => {
     assert(INTEGRATION_TEST_GITHUB_TOKEN)
+
+    // https://github.com/int128/list-associated-pull-requests-action/pull/491
     const outputs = await run({
       owner: 'int128',
       repo: 'list-associated-pull-requests-action',
@@ -31,5 +18,5 @@ describeIfGitHubToken('GitHub integration tests', () => {
       showOthersGroup: true,
     })
     expect(outputs).toMatchSnapshot()
-  }, 30000) // pr-491 contains many commits
+  }, 30000)
 })
