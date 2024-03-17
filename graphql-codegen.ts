2ac4988d1861064a63fb8c23fb2bcc14247e7265
Hidetake Iwata <int128@gmail.com>
2024-03-17 11:20:52 +0900

    Migrate to graphql-codegen.ts (#719)

---
+++ graphql-codegen.ts
@@ -0,0 +1,28 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import { schema } from '@octokit/graphql-schema'
+
+const config: CodegenConfig = {
+  schema: schema.idl,
+  documents: ['src/queries/**/*.ts'],
+  generates: {
+    'src/generated/graphql-types.ts': {
+      plugins: ['typescript'],
+    },
+    'src/generated/graphql.ts': {
+      preset: 'import-types',
+      plugins: ['typescript-operations'],
+      presetConfig: {
+        typesPath: './graphql-types',
+      },
+    },
+  },
+  config: {
+    // https://docs.github.com/en/graphql/reference/scalars
+    scalars: {
+      DateTime: 'string',
+      GitObjectID: 'string',
+    },
+  },
+}
+
+export default config
